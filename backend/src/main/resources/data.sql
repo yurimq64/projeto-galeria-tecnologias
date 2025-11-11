ec4ef84b19e96e5444aa54eab5d05cc9f552b6e8
yuri <ymendesqueiroz@gmail.com>
2025-11-11 11:35:25 -0300

    feat: popula o banco de dados via data.sql e ativa inicialização

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+TRUNCATE TABLE tecnologias RESTART IDENTITY;
+
+INSERT INTO tecnologias (nome) VALUES ('Java');
+INSERT INTO tecnologias (nome) VALUES ('Spring Boot');
+INSERT INTO tecnologias (nome) VALUES ('PostgreSQL');
+INSERT INTO tecnologias (nome) VALUES ('React');
+INSERT INTO tecnologias (nome) VALUES ('JavaScript');
+INSERT INTO tecnologias (nome) VALUES ('HTML5');
+INSERT INTO tecnologias (nome) VALUES ('CSS3');
+INSERT INTO tecnologias (nome) VALUES ('jQuery');
